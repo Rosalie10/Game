c0048e522b152e91cd6f7e9b5e9beb982b6f1544
Rosalie <vleemingrosalie@gmail.com>
2024-10-24 12:27:10 +0200

    Main window

---
+++ ChombBeats/ChombBeats/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ChombBeats
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
